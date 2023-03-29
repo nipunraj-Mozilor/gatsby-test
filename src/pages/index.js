a57dbbadc154f449684e6cc10d2e43255c31c901
nipunraj <nipunraj99@gmail.com>
2023-03-29 17:08:14 +0530

    youtube cookie

--- src/pages/index.js
+++ src/pages/index.js
@@ -172,7 +172,7 @@ const IndexPage = () => {
       <iframe
         width='560'
         height='315'
-        data-src='https://www.youtube.com/embed/lLfCvi6KVok'
+        src='https://www.youtube.com/embed/lLfCvi6KVok'
         title='YouTube video player'
         frameborder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
