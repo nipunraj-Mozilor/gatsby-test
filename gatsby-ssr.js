1077f79cd4ea283ed483e624c018a170fae1dfc7
nipunraj <nipunraj99@gmail.com>
2023-04-03 10:41:16 +0530

    cookieyes added

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 export function onRenderBody({ setHeadComponents }) {
   setHeadComponents([
     <script
-      id='cookieyes'
-      type='text/javascript'
-      src='https://cdn-cookieyes.com/client_data/d80f9cec632b20d257e5a646/script.js'
+      id="cookieyes"
+      type="text/javascript"
+      src="https://cdn-cookieyes.com/client_data/a447e2ace360d915f11ba5dd/script.js"
     ></script>,
   ]);
 }
