d305e29f3e19c28b582a99302cd069b832fa990c
nipunraj <nipunraj99@gmail.com>
2023-04-03 11:01:33 +0530

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
   siteMetadata: {
     title: `.`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      },
+    },
+  ],
+};
